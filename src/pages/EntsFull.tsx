7a26546c2b4f7d7eb3971aa13c05670576cae827
andylolu2 <andylolu24@gmail.com>
2022-06-21 14:38:14 +0100

    Reduce height to accomodate smaller screens

--- src/pages/EntsFull.tsx
+++ src/pages/EntsFull.tsx
@@ -8,7 +8,7 @@ import { solidStone, textBackground } from "../assets/img"
 import { Text } from "../components"
 
 const max = 9;
-const [cellHeight, cellWidth] = [55, 110];
+const [cellHeight, cellWidth] = [52, 110];
 const entsFull = program.entsFull;
 const locations = entsFull.map(({ location }) => location);
 
@@ -76,82 +76,75 @@ const EntsFull = () => (
       backgroundPosition: "center"
     }}
   >
-    <Box
-      py={4}
-      px={{
-        xs: 2,
-        sm: 4
-      }}
-      width="100%"
-      height="100%"
-      boxSizing="border-box"
-      display="flex"
-      justifyContent="start"
-      alignItems="center"
-    >
-      <Box display="flex" flexDirection="column">
-        <Cell pr={1} />
-        {locations.map((loc, i) => (
-          <Cell
-            pr={1}
-            key={i}
-            display="flex"
-            alignItems="center"
-            justifyContent="end"
-            borderRight={1}
-            borderColor="tertiary.main"
-          >
-            <Text textAlign="right">{loc}</Text>
-          </Cell>
-        ))}
-        <Cell pr={1} />
-      </Box>
-      <Box flex={1} sx={{ overflowX: "scroll", overflowY: "hidden" }}>
-        <Box display="flex">
-          {[...Array(max)].map((x, i) => {
-            const start = new Date("2022-06-21T20:30:00");
-            start.setHours(start.getHours() + i);
-            const time = start.toLocaleString("en-GB", {
-              hour: "numeric",
-              minute: "numeric",
-              hour12: true
-            });
-            return (
-              <Box flexBasis="content" key={i}>
-                <Cell display="flex" alignItems="center">
-                  <Text>{time}</Text>
-                </Cell>
-              </Box>
-            );
-          })}
+    <Box display="flex" alignItems="center" height="100%">
+      <Box
+        px={{
+          xs: 2,
+          sm: 4
+        }}
+        width="100%"
+        height="100%"
+        boxSizing="border-box"
+        display="flex"
+        justifyContent="start"
+        alignItems="center"
+      >
+        <Box display="flex" flexDirection="column">
+          <Cell pr={1} />
+          {locations.map((loc, i) => (
+            <Cell
+              pr={1}
+              key={i}
+              display="flex"
+              alignItems="center"
+              justifyContent="end"
+              borderRight={1}
+              borderColor="tertiary.main"
+            >
+              <Text textAlign="right">{loc}</Text>
+            </Cell>
+          ))}
         </Box>
-        {entsFull.map(({ location, ents, ents2 }, i) => (
-          <Box
-            position="relative"
-            display="flex"
-            width={max * cellWidth}
-            key={i}
-          >
-            {[...Array(max)].map((x, j) => (
-              <Box key={j}>
-                <Cell
-                  borderTop={1}
-                  borderLeft={j === 0 ? 0 : 1}
-                  borderRight={j === max - 1 ? 1 : 0}
-                  borderBottom={i === 10 ? 1 : 0}
-                  borderColor="tertiary.main"
-                />
-              </Box>
-            ))}
-            <Box position="absolute" height="100%">
-              {render({ ents, ents2 })}
-            </Box>
+        <Box flex={1} sx={{ overflowX: "scroll", overflowY: "hidden" }}>
+          <Box display="flex">
+            {[...Array(max)].map((x, i) => {
+              const start = new Date("2022-06-21T20:30:00");
+              start.setHours(start.getHours() + i);
+              const time = start.toLocaleString("en-GB", {
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true
+              });
+              return (
+                <Box flexBasis="content" key={i}>
+                  <Cell display="flex" alignItems="center">
+                    <Text>{time}</Text>
+                  </Cell>
+                </Box>
+              );
+            })}
           </Box>
-        ))}
-        <Box display="flex">
-          {[...Array(max)].map((x, i) => (
-            <Box flexBasis="content" key={i}>
-              <Cell />
+          {entsFull.map(({ location, ents, ents2 }, i) => (
+            <Box
+              position="relative"
+              display="flex"
+              width={max * cellWidth}
+              key={i}
+            >
+              {[...Array(max)].map((x, j) => (
+                <Box key={j}>
+                  <Cell
+                    borderTop={1}
+                    borderLeft={j === 0 ? 0 : 1}
+                    borderRight={j === max - 1 ? 1 : 0}
+                    borderBottom={i === 10 ? 1 : 0}
+                    borderColor="tertiary.main"
+                  />
+                </Box>
+              ))}
+              <Box position="absolute" height="100%">
+                {render({ ents, ents2 })}
+              </Box>
             </Box>
           ))}
         </Box>
